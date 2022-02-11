56b02c074d586b076acacea4505b6f7b8ab6f029
Louis331 <louiskirkpatrick@gmail.com>
2022-02-11 18:25:54 +0000

    function for bacth adding words

--- pages/api/words.js
+++ pages/api/words.js
@@ -1,8 +1,16 @@
-import { getAllWords } from "../../services/fireBase"
+import { getAllWords, addWord } from "../../services/fireBase"
 
 export default async function handler(req, res) {
   if (req.method === 'GET') {
     res.status(200).json({ words: await getAllWords() })
+  } else if (req.method === 'POST') {
+    let words = req.body.words
+
+    words.forEach(word => {
+        addWord(word)        
+    });
+
+    res.status(200).json({response: 'Words added'})
   } else {
     res.status(405).json({ error: 'Method not allowed' })
   }
