04fa4227b983804040fa593a48cfe0f6ba34cd47
Louis331 <louiskirkpatrick@gmail.com>
2022-02-11 17:14:15 +0000

    added more testing

--- services/fireBase.js
+++ services/fireBase.js
@@ -43,7 +43,17 @@ export async function getAllWords(){
     let doucmentRef = doc(db, process.env.WORD_TABLE, 'words')
     let documentSnapshot = await getDoc(doucmentRef)
 
-    return process.env.FIREBASE_AUTH_DOMAIN
+    let test = {
+        apiKey: process.env.FIREBASE_KEY,
+        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.FIREBASE_APP_ID,
+        row: process.env.WORD_TABLE
+    }
+
+    return test
 
     // return documentSnapshot.data()['words']
 }
